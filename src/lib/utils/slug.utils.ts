a000da2d65b9b7e9d663d4503aa971cddf130062
Diego González Liarte <diegogliarte@gmail.com>
2026-03-23 20:29:20 +0100

    better slugify

--- src/lib/utils/slug.utils.ts
+++ src/lib/utils/slug.utils.ts
@@ -1,3 +1,10 @@
 export function slugify(s: string): string {
-	return s.toLowerCase().replace(/\s+/g, '-');
+	return s
+		.toLowerCase()
+		.normalize('NFD') // split accented chars (é → e + ́)
+		.replace(/[\u0300-\u036f]/g, '') // remove diacritics
+		.replace(/[^a-z0-9\s-]/g, '') // remove weird chars
+		.trim()
+		.replace(/\s+/g, '-') // spaces -> dashes
+		.replace(/-+/g, '-'); // collapse multiple dashes
 }
