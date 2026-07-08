832d0541b78ebedb7c6b5ba88b2fe2014b7452a7
Diego González Liarte <diegogliarte@gmail.com>
2026-07-08 22:16:02 +0200

    add icon padz

--- src/lib/tools/puzzle-and-dragons-z/index.ts
+++ src/lib/tools/puzzle-and-dragons-z/index.ts
@@ -2,5 +2,5 @@ import type { ToolCategoryMetadata } from '$lib/tools/types';
 
 export const category: ToolCategoryMetadata = {
 	title: 'Puzzle & Dragons Z',
-	favicon: '/puzzle-and-dragons-z/icons/16.png'
+	favicon: '/favicons/puzzle_and_dragons.ico'
 };
